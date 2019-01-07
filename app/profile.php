e97c3ee7664779e125d14dd9cd07480fd123159e
simo <jsimo1@yahoo.com>
2019-01-07 01:54:36 +0100

    next

--- app/profile.php
+++ app/profile.php
@@ -2,13 +2,10 @@
 
 namespace App;
 
-use App\Profile;
-use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    
     public function user()
     {
        return $this->belongsTo(User::class);
